10c179169c9366de3cbab4b425e90162fa968d0f
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-01 21:45:08 +0530

    indentatioon correction

--- 04props/src/App.jsx
+++ 04props/src/App.jsx
@@ -2,11 +2,9 @@ import './App.css';
 import Card from './components/Card';
 function App() {
 
-return (
-  <Card username="Naman Aggarwal"/>
-)
-
-  
+    return (
+      <Card username="Naman Aggarwal"/>
+    )
 }
 
 export default App
