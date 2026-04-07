c3cfda45ff710d0a4c225376226f4b9e903ca8d6
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-07 23:15:46 +0530

    context understanding

---
+++ 09miniContext/src/components/Login.jsx
@@ -0,0 +1,25 @@
+import {useState, useContext} from "react";
+import UserContext from "../context/UserContext";
+
+export default function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {setUser} = useContext(UserContext);
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setUser({username, password});
+    }
+
+    return(
+        <div>
+            <h2>Login</h2>
+            <input type='text' value = {username} onChange={(e)=>{return setUsername(e.target.value)}} placeholder="username" />
+            <br />
+            <input type='text' value = {password} onChange={(e)=>{return setPassword(e.target.value)}} placeholder="password" />
+            <br />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
